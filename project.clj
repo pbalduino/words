10551e61dde02c167071e7330b6fdfac0564241b
Plinio Balduino <pbalduino+github@gmail.com>
2013-11-11 19:01:47 -0200

    Added trace for debug stuff

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.trace "0.7.6"]]
   :source-paths ["src/clojure"]
 ;  :java-source-paths ["src/java"]
   :main words.core)
